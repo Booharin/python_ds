057eaa908a25266fd9cf088b9a47fabc65ea5f67
Alexandr <booharin@bk.ru>
2020-05-05 20:02:25 +0300

    Updated les_1

--- les_1.py
+++ les_1.py
@@ -1,6 +1,23 @@
 import numpy as np
 
-a = np.array([[1, 2, 3, 3, 1], [6, 8, 11, 10, 7]])
+# Topic 1
+
+# 1
+a = np.array([[1, 2, 3, 3, 1],
+              [6, 8, 11, 10, 7]])
 a.transpose()
-mean_a = a.mean(axis = 1)
+mean_a = a.mean(axis=1)
 print(mean_a)
+
+# 2
+a_centered = (a - mean_a[:, None]).transpose()
+print(a_centered)
+
+# 3
+a_centered_sp = a_centered[:, 0].dot(a_centered[:, 1])
+# число наблюдений 2
+x = a_centered_sp / (2 - 1)
+print(x)
+
+# 4
+print(np.cov(a)[0, 1])
