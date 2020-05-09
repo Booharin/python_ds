993dc52aae6ddf7e50731d4388879e3010ea0341
Alexandr <booharin@bk.ru>
2020-05-09 18:49:07 +0300

    Updated les_1

--- les_1.py
+++ les_1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 # Topic 1
 
@@ -21,3 +22,88 @@ print(x)
 
 # 4
 print(np.cov(a)[0, 1])
+print("---------------------------------")
+
+# Topic 2
+
+# 1
+authors = pd.DataFrame({
+    "author_id": [1, 2, 3],
+    "author_name": ['Тургенев', 'Чехов', 'Островский']
+})
+
+authors = pd.DataFrame(authors)
+
+book = pd.DataFrame({
+    "author_id": [
+        1,
+        1,
+        1,
+        2,
+        2,
+        3,
+        3
+    ],
+    "book_title": [
+        'Отцы и дети',
+        'Рудин',
+        'Дворянское гнездо',
+        'Толстый и тонкий',
+        'Дама с собачкой',
+        'Гроза',
+        'Таланты и поклонники'
+    ],
+    "price": [
+        450,
+        300,
+        350,
+        500,
+        450,
+        370,
+        290
+    ]
+})
+
+print(book)
+print("---------------------------------")
+
+# 2
+authors_price = pd.merge(authors, book, on='author_id', how='outer')
+
+# 3
+top5 = authors_price.nlargest(5, "price")
+print(top5)
+print("---------------------------------")
+
+# 4
+authors_stat = pd.DataFrame({
+    "author_name": authors_price["author_name"],
+    "min_price": authors_price["price"].min(),
+    "max_price": authors_price["price"].max(),
+    "mean_price": authors_price["price"].mean().astype(int)
+})
+print(authors_stat)
+print("---------------------------------")
+
+# 5
+cover = pd.DataFrame(
+    {'cover': ['твердая', 'мягкая', 'мягкая', 'твердая', 'твердая', 'мягкая', 'мягкая']}
+)
+
+authors_price = pd.concat([authors_price, cover], axis=1)
+print(authors_price)
+print("---------------------------------")
+
+book_info = pd.pivot_table(authors_price,
+                           values=['price'],
+                           index=['author_name'],
+                           columns=['cover'],
+                           aggfunc=np.sum,
+                           fill_value=0)
+print(book_info)
+
+book_info.to_pickle("book_info.pkl")
+book_info2 = pd.read_pickle("book_info.pkl")
+print("---------------------------------")
+
+print(book_info.equals(book_info2))
