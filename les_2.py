1036f17c481514faaa8f82592a87f6c06f736230
Alexandr <booharin@bk.ru>
2020-05-18 00:21:47 +0300

    Updated les_2

--- les_2.py
+++ les_2.py
@@ -39,4 +39,32 @@ plt.ylabel("Функция f(x)", fontdict=label_font)
 
 plt.axis([0.5, 9.5, -2.5, 2.5])
 
+# plt.show()
+
+# 3
+
+x = np.linspace(-3, 3, 51)
+
+y1 = x ** 2
+y2 = 2 * x + 0.5
+y3 = -3 * x - 1.5
+y4 = [np.sin(element) for element in x]
+
+fig, ax = plt.subplots(nrows=2, ncols=2)
+fig.set_size_inches(8.5, 6)
+fig.subplots_adjust(wspace=0.3)
+
+ax[0, 0].plot(x, y1)
+ax[0, 0].set(title = 'График y1')
+ax[0, 0].set_xlim([-5, 5])
+ax[1, 0].plot(x, y2)
+ax[1, 0].set(title = 'График y2')
+ax[0, 1].plot(x, y3)
+ax[0, 1].set(title = 'График y3')
+ax[1, 1].plot(x, y4)
+ax[1, 1].set(title = 'График y4')
+
 plt.show()
+
+
+
