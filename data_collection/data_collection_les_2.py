3ad1e4d6e4fb18d0f4b2523508d6e8feb6044834
Alexandr <booharin@bk.ru>
2020-07-01 19:21:39 +0300

    Updated les_2

--- data_collection/data_collection_les_2.py
+++ data_collection/data_collection_les_2.py
@@ -2,44 +2,85 @@ from bs4 import BeautifulSoup as bs
 import requests
 from pprint import pprint
 import pandas as pd
+import numpy as np
+from typing import List
 
 
 def getSuperJobVacanciesDescription():
     superjob_url = 'https://www.superjob.ru'
     name = input('Введите название вакансии латинскими буквами: ')
-    # интерсно почему,
-    # возвращает правильное количество вакансий только с прараметром geo%5Bt%5D%5B0%5D=4 (Москва например)
-    # хотя сам сайт возвращает все вакансии и без этого парметра
-    request_url = f'{superjob_url}/vakansii/{name}.html?geo%5Bt%5D%5B0%5D=4'
 
-    response = requests.get(request_url).text
-    soup = bs(response, 'lxml')
-    vacancies_list = soup.find_all('div', {'class': 'Fo44F QiY08 LvoDO'})
-    pprint(len(vacancies_list))
-
+    path = f'/vakansii/{name}.html?geo%5Bt%5D%5B0%5D=4'
+    request_url = f'{superjob_url}{path}'
+    page = 0
+    pages_max = 2
     vacancies = []
-    for vacancy in vacancies_list:
-        vacancy_data = {
-            'name': vacancy.find('a').getText(),
-            'salary': vacancy.find('span', {'class': '_3mfro _2Wp8I PlM3e _2JVkc _2VHxz'})
-                .getText()
+
+    while page < 2:
+
+        response = requests.get(request_url).text
+        soup = bs(response, 'lxml')
+        vacancies_list = soup.find_all('div', {'class': 'Fo44F QiY08 LvoDO'})
+        pprint(len(vacancies_list))
+
+        for vacancy in vacancies_list:
+            vacancy_data = {
+                'name': vacancy.find('a').getText(),
+            }
+
+            # salary
+            salary_text = vacancy.find('span', {'class': '_3mfro _2Wp8I PlM3e _2JVkc _2VHxz'})\
+                .getText()\
                 .replace('\xa0', '')
-        }
+            salary_list: List[str] = getSalaryFromText(salary_text, '—')
+            vacancy_data['salary_min'] = salary_list[0]
+            vacancy_data['salary_max'] = salary_list[1]
+            vacancy_data['salary_currency'] = salary_list[2]
+            vacancy_data['salary_text'] = salary_text
 
-        links = vacancy.find_all('a')
-        vacancy_data['link'] = superjob_url + links[0]['href']
-        vacancy_data['source_link'] = superjob_url + links[1]['href']
+            # links
+            links = vacancy.find_all('a')
+            vacancy_data['link'] = superjob_url + links[0]['href']
+            vacancy_data['source_link'] = superjob_url + links[1]['href']
 
-        company = vacancy.find('div', {'class': '_3_eyK _3P0J7 _9_FPy'})
-        vacancy_data['company_name'] = company.find('span').getText()
+            company = vacancy.find('div', {'class': '_3_eyK _3P0J7 _9_FPy'})
+            vacancy_data['company_name'] = company.find('span').getText()
 
-        address = vacancy.find('span', {'class': '_3mfro f-test-text-company-item-location _9fXTd _2JVkc _2VHxz'})
-        vacancy_data['address'] = address.find_all('span')[2].getText().replace('\xa0', '')
+            address = vacancy.find('span', {'class': '_3mfro f-test-text-company-item-location _9fXTd _2JVkc _2VHxz'})
+            vacancy_data['address'] = address.find_all('span')[2].getText().replace('\xa0', '')
 
-        vacancies.append(vacancy_data)
+            vacancies.append(vacancy_data)
+
+        next_page_tag = soup.find('a', {'class': 'f-test-button-dalshe'})
+        if next_page_tag is None:
+            page = pages_max
+        else:
+            path = next_page_tag['href']
+            page += 1
 
     v = pd.DataFrame(vacancies)
     print(v.to_string())
+
+
+def getSalaryFromText(text, separator) -> List[str]:
+    currency = 'руб.'
+    salary_list = [None, None, currency]
+    if text[-4:] == currency:
+        text = text.replace(currency, '')
+
+        if separator in text:
+            text_list = text.split(separator)
+            salary_list[0] = text_list[0]
+            salary_list[1] = text_list[1]
+        elif 'от' in text:
+            salary_list[0] = text.replace('от', '')
+        elif 'до' in text:
+            salary_list[1] = text.replace('до', '')
+
+    else:
+        salary_list[2] = None
+
+    return salary_list
 
 
 def get_hh_vacancies_description():
@@ -49,25 +90,48 @@ def get_hh_vacancies_description():
         'Accept': '*/*'}
     name = input('Введите название вакансии: ')
 
-    request_url = f'{hh_url}/search/vacancy?area=1&st=searchVacancy&text={name}&fromSearch=true'
-    response = requests.get(request_url, headers=header).text
-
-    soup = bs(response, 'lxml')
-    vacancies_list = soup.find_all('div', {'class': 'vacancy-serp-item'})
-    pprint(len(vacancies_list))
-
+    path = f'/search/vacancy?area=1&st=searchVacancy&text={name}&fromSearch=true'
+    request_url = f'{hh_url}{path}'
+    page = 0
+    pages_max = 2
     vacancies = []
-    for vacancy in vacancies_list:
-        vacancy_data = {'name': vacancy.find('div', {'class': 'vacancy-serp-item__info'}).getText(),
-                        'salary': vacancy.find('div', {'class': 'vacancy-serp-item__sidebar'}).getText(),
-                        'link': vacancy.find('a', {'class': 'bloko-link HH-LinkModifier'})['href']}
 
-        employer_info = vacancy.find('div', {'class': 'vacancy-serp-item__meta-info'})
-        vacancy_data['employer_link'] = hh_url + employer_info.find('a')['href']
-        vacancy_data['company_name'] = employer_info.getText()
-        vacancy_data['address'] = vacancy.find('span', {'class': 'vacancy-serp-item__meta-info'}).getText()
+    while page < 2:
 
-        vacancies.append(vacancy_data)
+        response = requests.get(request_url, headers=header).text
+
+        soup = bs(response, 'lxml')
+        vacancies_list = soup.find_all('div', {'class': 'vacancy-serp-item'})
+        pprint(len(vacancies_list))
+
+        for vacancy in vacancies_list:
+            vacancy_data = {'name': vacancy.find('div', {'class': 'vacancy-serp-item__info'}).getText(),
+                            'link': vacancy.find('a', {'class': 'bloko-link HH-LinkModifier'})['href']}
+
+            # salary
+            salary_text = vacancy.find('div', {'class': 'vacancy-serp-item__sidebar'})\
+                .getText()\
+                .replace('\xa0', '')\
+                .replace(' ', '')
+            salary_list: List[str] = getSalaryFromText(salary_text, '-')
+            vacancy_data['salary_min'] = salary_list[0]
+            vacancy_data['salary_max'] = salary_list[1]
+            vacancy_data['salary_currency'] = salary_list[2]
+            vacancy_data['salary_text'] = salary_text
+
+            employer_info = vacancy.find('div', {'class': 'vacancy-serp-item__meta-info'})
+            vacancy_data['employer_link'] = hh_url + employer_info.find('a')['href']
+            vacancy_data['company_name'] = employer_info.getText()
+            vacancy_data['address'] = vacancy.find('span', {'class': 'vacancy-serp-item__meta-info'}).getText()
+
+            vacancies.append(vacancy_data)
+
+        next_page_tag = soup.find('a', {'data-qa': 'pager-next'})
+        if next_page_tag is None:
+            page = pages_max
+        else:
+            path = next_page_tag['href']
+            page += 1
 
     v = pd.DataFrame(vacancies)
     print(v.to_string())
